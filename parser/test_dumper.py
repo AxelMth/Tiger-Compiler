3d225ccb74ced55d63f6a20bb3511239a5201cb7
Axel MATHIEU <axel.mathieu@telecom-paristech.fr>
2017-02-27 14:20:32 +0100

    WIP

--- parser/test_dumper.py
+++ parser/test_dumper.py
@@ -22,5 +22,9 @@ class TestDumper(unittest.TestCase):
         self.check("5-2/3","(5 - (2 / 3))")
         self.check("10-2-1","((10 - 2) - 1)")
 
+    def test_ifThenElse(self):
+        self.check("if 1 then 2 else 2+1","if 1 then 2 else (2 + 1)")
+        self.check("if 1-1 then 2 else 5+2","if (1 - 1) then 2 else (5 + 2)")
+
 if __name__ == '__main__':
     unittest.main()
