57ec8f4dc2166513e91e0f364e1c779ee6c16880
Axel MATHIEU <axel.mathieu@telecom-paristech.fr>
2017-03-31 11:26:16 +0200

    Deuxième implémentation de blocks.py

--- ir/blocks.py
+++ ir/blocks.py
@@ -6,22 +6,29 @@ def reorder_blocks(seq, frame):
     """Reorder blocks in seq so that the negative branch of a CJUMP always
     follows the CJUMP itself. frame is the frame of the corresponding
     function."""
+    # Sorting blocks
     assert(isinstance(seq, SEQ))
     assert(isinstance(frame, Frame))
-    blocks = {}
+    blocks = []
     inside_block = False
+    index = 0
     for stm in seq.stms:
         if isinstance(stm,LABEL):
             if inside_block:
-                blocks[current_label] = JUMP(NAME(stm.label.name))
+                blocks[index] += JUMP(NAME(stm.label))
             current_label = stm.label.name
-            blocks[current_label] = []
+            blocks[index] = [current_label]
             inside_block = True
         elif inside_block:
             if isinstance(stm,CJUMP) or isinstance(stm,JUMP):
-                blocks[current_label].append(stm)
+                blocks[index] += stm
                 inside_block = False
+                index += 1
             elif not isinstance(stm,LABEL):
-                blocks[current_label].append(stm)
+                blocks[index] += stm
     print(blocks)
+    # Trace determination
+    blocks_examined = {}
+
+
     return seq
