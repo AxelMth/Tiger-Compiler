d2ebcb4e25d8d0a19fd1092e29e230a81a8035b8
Axel MATHIEU <axel.mathieu@telecom-paristech.fr>
2017-03-31 10:56:36 +0200

    Implémentation de blocks.py

--- ir/blocks.py
+++ ir/blocks.py
@@ -8,5 +8,20 @@ def reorder_blocks(seq, frame):
     function."""
     assert(isinstance(seq, SEQ))
     assert(isinstance(frame, Frame))
-    # Implement here
+    blocks = {}
+    inside_block = False
+    for stm in seq.stms:
+        if isinstance(stm,LABEL):
+            if inside_block:
+                blocks[current_label] = JUMP(NAME(stm.label.name))
+            current_label = stm.label.name
+            blocks[current_label] = []
+            inside_block = True
+        elif inside_block:
+            if isinstance(stm,CJUMP) or isinstance(stm,JUMP):
+                blocks[current_label].append(stm)
+                inside_block = False
+            elif not isinstance(stm,LABEL):
+                blocks[current_label].append(stm)
+    print(blocks)
     return seq
